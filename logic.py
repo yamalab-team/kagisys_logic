f06cdf5e973a217e3b30f25ba43137aac07c80cd
AkamaKousei <s16003@tokyo.kosen-ac.jp>
2016-12-06 17:12:37 +0900

    [change]サーボモータ制御クラスの作成。今までのプログラムをサーボモータ制御クラスに追加。

--- logic.py
+++ logic.py
@@ -7,55 +7,62 @@ import signal
 import sys
 import nfc
 
-#鍵の番号
-key_list = ["0114B405B6116944","01010114DD15E70D"]
 
-#終了の際の処理
-def exit_handler(signal, frame):
-	print("\nExit")
-	GPIO.cleanup()
-	sys.exit(0)
+class ControlServomotor():
+	"""サーボモータの制御"""
+	#メイン-----------------------------------------------------------------------
+	def __init__(self):
+		#鍵の番号
+		self.key_list = ["0114B405B6116944","01010114DD15E70D"]
 
-#基本的なセッティング
-signal.signal(signal.SIGINT, exit_handler)
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(12, GPIO.OUT)
-toggle = True
+		#基本的なセッティング
+		signal.signal(signal.SIGINT, self.exit_handler)
+		GPIO.setmode(GPIO.BCM)
+		GPIO.setup(12, GPIO.OUT)
+		self.toggle = True
 
-#タッチされたとき
-def touched(tag):
-    #変数のグローバル化
-	global toggle
-	global key_list
-	#idの照合
-	tag_id = tag.identifier.encode("hex").upper()
-	print(tag_id)
-	if tag_id not in key_list:
-	    #データが正しいidと異なっていた場合
-		print("No matching Key")
-		return
+		clf = nfc.ContactlessFrontend('usb')
 
-	servo = GPIO.PWM(12, 50)
-	if toggle:
-        #鍵の解錠
-		servo.start(7.7)
-		print("Open")
-		time.sleep(0.5)
-		toggle = False
-	else:
-        #鍵の施錠
-		servo.start(2.5)
-		print("Close")
-		time.sleep(0.5)
-		toggle = True
-	servo.stop()
+		print("setting OK.")
 
-clf = nfc.ContactlessFrontend('usb')
+		#繰り返し
+		while True:
+			clf.connect(rdwr={'on-connect': self.touched})
+			time.sleep(3)
+			print("relese")
 
-print("setting OK.")
 
-#繰り返し
-while True:
-	clf.connect(rdwr={'on-connect': touched})
-	time.sleep(3)
-	print("relese")
+	#終了の際の処理---------------------------------------------------------------
+	def exit_handler(self,signal, frame):
+		print("\nExit")
+		GPIO.cleanup()
+		sys.exit(0)
+
+
+	#タッチされたときの処理-----------------------------------------------------------
+	def touched(self,tag):
+		#idの照合
+		tag_id = tag.identifier.encode("hex").upper()
+		print(tag_id)
+		if tag_id not in self.key_list:
+		    #データが正しいidと異なっていた場合
+			print("No matching Key")
+			return
+
+		servo = GPIO.PWM(12, 50)
+		if self.toggle:
+	        #鍵の解錠
+			servo.start(7.7)
+			print("Open")
+			time.sleep(0.5)
+			self.toggle = False
+		else:
+	        #鍵の施錠
+			servo.start(2.5)
+			print("Close")
+			time.sleep(0.5)
+			self.toggle = True
+		servo.stop()
+
+#クラスの呼び出し
+ControlServomotor()
