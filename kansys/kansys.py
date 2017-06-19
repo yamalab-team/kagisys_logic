9586a0362cbe6b7c59b74199518b7251bea37e88
akakou <akakou571@gmail.com>
2017-06-20 08:07:35 +0900

    [add] kansysのpreversion完成

--- kansys/kansys.py
+++ kansys/kansys.py
@@ -1,22 +1,41 @@
 # coding:utf-8
+import RPi.GPIO as GPIO
 
 import cv2
 import configparser
 from slacker import Slacker
+
+import time
 
 
 class Kansys():
     '''Kansys用クラス'''
     def __init__(self):
         '''セッティング'''
+        # config
         config = configparser.SafeConfigParser()
         config.read('/home/pi/project/kagisys.conf')
-        token = config.get('Slack', 'kansys-url')
 
-        self.channel_id = config.get('Slack', 'channel_id')
-        self.channel_name = config.get('Slack', 'channel_name')
+        # gpio
+        self.pin = config.get('Kansys', 'gpio-pin')
+        GPIO.setup()
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.pin, GPIO.IN)
+
+        # slack
+        token = config.get('Kansys', 'slack-url')
+        self.channel_id = config.get('Kansys', 'channel_id')
+        self.channel_name = config.get('Kansys', 'channel_name')
 
         self.slack = Slacker(token)
+
+    def run(self):
+        while True:
+            status = GPIO.input(self.pin)
+            if(status):
+                time.sleep(1)
+                self.take_picture()
+                self.send_to_slack()
 
     def send_to_slack(self):
         '''ファイルの送信＆ピン止め'''
