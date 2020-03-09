c507601148782695850a668a7b6fe10f550c8103
sun-yryr <taittide@gmail.com>
2020-03-09 16:47:01 +0900

    a

--- main.py
+++ main.py
@@ -4,22 +4,22 @@ from lib import Kagi, Led, OLED_Display, Slack, Observer
 from auth_nfc.kagisys_nfc import NFC_Reader
 
 class Kagisys(Observer):
-    __DEFAULT = "defalut"
-    __AUTHORIZE = "auhorize"
-    __ADDNEWUSER = "addnewuser"
+    _DEFAULT = "defalut"
+    _AUTHORIZE = "auhorize"
+    _ADDNEWUSER = "addnewuser"
     MODE = __DEFAULT 
     def __init__(self):
         super().__init__()
     
     def migration_to_authorization(self):
-        self.MODE = self.__AUTHORIZE
+        self.MODE = self._AUTHORIZE
         self.notify()
     def migration_to_adduser(self, u_id):
-        self.MODE = self.__ADDNEWUSER
+        self.MODE = self._ADDNEWUSER
         self.authorization_uid = u_id
         self.notify()
     def migration_to_default(self):
-        self.MODE = self.__DEFAULT
+        self.MODE = self._DEFAULT
         self.authorization_uid = None
         self.notify()
 
@@ -30,7 +30,7 @@ def main():
         if not idm:
             continue
         res = n.authorization(idm=idm)
-        if kagisys.MODE == kagisys.__DEFAULT:
+        if kagisys.MODE == kagisys._DEFAULT:
             if not res:
                 # 登録されていないもの
                 s.post("未登録のNFCカード：" + idm)
@@ -41,7 +41,7 @@ def main():
                 k.lock(u_name=u_name)
             else:
                 k.open(u_name=u_name)
-        elif kagisys.MODE == kagisys.__AUTHORIZE:
+        elif kagisys.MODE == kagisys._AUTHORIZE:
             # 認証処理
             if not res:
                 led_module.error()
@@ -49,7 +49,7 @@ def main():
                 continue
             (u_id, u_name) = res
             kagisys.migration_to_adduser(u_id=u_id)
-        elif kagisys.MODE == kagisys.__ADDNEWUSER:
+        elif kagisys.MODE == kagisys._ADDNEWUSER:
             # 追加処理
             if not res:
                 n.addUser(idm=idm, u_id=kagisys.authorization_uid)
